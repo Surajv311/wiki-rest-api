9767e11e304513f3dc3b113b24a378a648b3ff19
Surajv <vsuraj2405@gmail.com>
2020-08-15 23:25:52 +0530

    setting up post route

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ content : String
 const Article = mongoose.model("Article" , articleSchema);
 
 // create get route to fetch articles
-
+// EARLIER GET/POST WAS DONE USING <form> TAG IN HTML... NOW WE ARE USING VIA API'S
 app.get("/articles" , function(req,res){
 // here we will query our db & find all the docs inside article collection
 Article.find(function(err, foundArticles){
@@ -37,7 +37,26 @@ else{
 });
 
 });
+// now in case of post route... we have to post data without building frontend
+// purely on basis of api
+// to use express to post request....
+app.post("/articles" , function(req, res){
+// if <form> then we may tap into individual pieces of data aka 'name'
+console.log( );
+console.log();
 
+const newArticle = new Article({
+title : req.body.title,
+content : req.body.content
+
+});
+
+newArticle.save()
+
+
+// this must save it in db by robo3T
+
+});
 
 
 
