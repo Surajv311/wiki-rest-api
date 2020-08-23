1d7d5885bf2583803ffd212657258598868e491f
Surajv <vsuraj2405@gmail.com>
2020-08-23 14:09:03 +0530

    updated js for REST api

--- app.js
+++ app.js
@@ -14,55 +14,115 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/robo3TwikiDB", {useNewUrlParser:true});
+mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true});
+
 const articleSchema = {
-title : String ,
-content : String
+  title: String,
+  content: String
 };
 
-const Article = mongoose.model("Article" , articleSchema);
+const Article = mongoose.model("Article", articleSchema);
 
-// create get route to fetch articles
-// EARLIER GET/POST WAS DONE USING <form> TAG IN HTML... NOW WE ARE USING VIA API'S
-app.get("/articles" , function(req,res){
-// here we will query our db & find all the docs inside article collection
-Article.find(function(err, foundArticles){
-if(!err){
-res.send(foundArticles);
-}
-else{
-  res.send(err);
-}
+///////////////////////////////////Requests Targetting all Articles////////////////////////
 
+app.route("/articles")
+
+.get(function(req, res){
+  Article.find(function(err, foundArticles){
+    if (!err) {
+      res.send(foundArticles);
+    } else {
+      res.send(err);
+    }
+  });
+})
+
+.post(function(req, res){
+
+  const newArticle = new Article({
+    title: req.body.title,
+    content: req.body.content
+  });
+
+  newArticle.save(function(err){
+    if (!err){
+      res.send("Successfully added a new article.");
+    } else {
+      res.send(err);
+    }
+  });
+})
+
+.delete(function(req, res){
+
+  Article.deleteMany(function(err){
+    if (!err){
+      res.send("Successfully deleted all articles.");
+    } else {
+      res.send(err);
+    }
+  });
 });
 
+////////////////////////////////Requests Targetting A Specific Article////////////////////////
+
+app.route("/articles/:articleTitle")
+
+.get(function(req, res){
+
+  Article.findOne({title: req.params.articleTitle}, function(err, foundArticle){
+    if (foundArticle) {
+      res.send(foundArticle);
+    } else {
+      res.send("No articles matching that title was found.");
+    }
+  });
+})
+
+.put(function(req, res){
+
+  Article.update(
+    {title: req.params.articleTitle},
+    {title: req.body.title, content: req.body.content},
+    {overwrite: true},
+    function(err){
+      if(!err){
+        res.send("Successfully updated the selected article.");
+      }
+    }
+  );
+})
+
+.patch(function(req, res){
+
+  Article.update(
+    {title: req.params.articleTitle},
+    {$set: req.body},
+    function(err){
+      if(!err){
+        res.send("Successfully updated article.");
+      } else {
+        res.send(err);
+      }
+    }
+  );
+})
+
+.delete(function(req, res){
+
+  Article.deleteOne(
+    {title: req.params.articleTitle},
+    function(err){
+      if (!err){
+        res.send("Successfully deleted the corresponding article.");
+      } else {
+        res.send(err);
+      }
+    }
+  );
 });
-// now in case of post route... we have to post data without building frontend
-// purely on basis of api
-// to use express to post request....
-app.post("/articles" , function(req, res){
-// if <form> then we may tap into individual pieces of data aka 'name'
-console.log( );
-console.log();
-
-const newArticle = new Article({
-title : req.body.title,
-content : req.body.content
-
-});
-
-newArticle.save()
 
 
-// this must save it in db by robo3T
-
-});
-
-// for delete.....
-
-app.delete("",function(req,res){
-  ///code ////
-});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
